b95ccfa61e17a3fa23139f1a8a9c28ea643348bc
AwaOdori <https://www.google.com/search?q=xcode+add+package+dependency+missing&rlz=1C5CHFA_enJP976JP976&sxsrf=APq-WBvtrCupSHiMRKj0IROwJv6zWeqDig%3A1643277141923&ei=VWvyYbXWN7-q0PEP7sOI0AU&oq=xcode+add+package+dependency+&gs_lcp=Cgdnd3Mtd2l6EAEYADIECAAQEzIECAAQEzIECAAQEzIGCAAQHhATMgYIABAeEBMyCAgAEAgQHhATMggIABAIEB4QEzIICAAQCBAeEBM6CAgAEIAEELADOgcIABCwAxAeOgkIABCwAxAIEB46BQghEKABSgQIQRgBSgQIRhgAUIoEWPQSYLkaaAJwAHgAgAGrAYgBigOSAQMxLjKYAQCgAQHIAQfAAQE&sclient=gws-wiz>
2023-09-30 13:12:19 +0900

    Initial Commit

---
+++ CoRatiOS/CoRatiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoRatiOSApp.swift
+//  CoRatiOS
+//
+//  Created by Yuki-OHMORI on 2023/09/30.
+//
+
+import SwiftUI
+
+@main
+struct CoRatiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
